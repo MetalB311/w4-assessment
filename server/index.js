9acdf963de76d0a62cc42bba58319b85bad6a35e
MetalB <metalb311@gmail.com>
2022-10-27 22:06:46 -0600

    couple notes for your understanding your main issue

--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ app.use(cors());
 
 app.use(express.json());
 
+//calling the controller file for reference 
 const { getCompliment } = require('./controller')
 const { getFortune } = require('./controller')
 const { getChore } = require('./controller')
 const { addChore} = require('./controller')
 const { getActivity } = require('./controller')
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+//All of these require the same so they can be written like this
+// const {getCompliment, getFortune, getChore, addChore, getActivity} = require('./controller')
+//******************************************************** */
 
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
